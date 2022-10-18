a8d721c630b4a56b58d7b0b66cc0fffc826fc3fd
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-18 21:59:40 +0200

    middelware

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ use App\Http\Controllers\Controller;
 */
 
 // Basic Route
-Route::get('/index', function () {
-    return 'Hello World';
-});
+// Route::get('/index', function () {
+//     return 'Hello World';
+// });
 
 //controller route
 
-Route::get('/hello', [Controller::class, 'index']);
+// Route::get('/hello', [Controller::class, 'index']);
 
 
 
 
 Route::get('/request', function () {
     return view('request');
-});
+})->middleware('test');
 
 
 
@@ -38,12 +38,12 @@ Route::get('/request', function () {
 
 
 //route with request
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
  
-Route::any('/user', function (Request $request) {
-    $name = $request->input("name");
-    return $name;
-});
+// Route::any('/user', function (Request $request) {
+//     $name = $request->input("name");
+//     return $name;
+// });
 
 
 
@@ -56,20 +56,20 @@ Route::any('/user', function (Request $request) {
 
 
 //route with 2 methode 
-Route::match(['get', 'post'], '/', function () {
-});
+// Route::match(['get', 'post'], '/', function () {
+// });
  
 
 // route any methode
-Route::any('/', function () {
-});
+// Route::any('/', function () {
+// });
 
 //redirect route
-Route::redirect('/her', 'hello');
+// Route::redirect('/her', 'hello');
 
 
 
 //route with id
-Route::get('/person/{id}', function ($id) {
-    return 'person id : '.$id;
-});
+// Route::get('/person/{id}', function ($id) {
+//     return 'person id : '.$id;
+// });
