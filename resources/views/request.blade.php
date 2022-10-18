4b0db64c2f5fd85ed30e04ce25eef19578280d4e
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-18 07:01:20 -0700

    csrf

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,4 +1,5 @@
 <form action="/user" method="get">
+    {{-- include a CSRF token field for protected and validate request  --}}
     @csrf
     <input type="text" name="name">
     <button>click</button>
