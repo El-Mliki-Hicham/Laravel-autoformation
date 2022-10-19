0163c0a8afaaec8c637f8fca0579009246520b4c
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-19 01:44:37 -0700

    middelware

--- The_basics/Middelware/routes/web.php
+++ The_basics/Middelware/routes/web.php
@@ -18,50 +18,6 @@ use Illuminate\Http\Request;
 |
 */
 
-// Basic Route
-// Route::get('/index', function () {
-//     return 'Hello World';
-// });
-
-//controller route
-
-// Route::get('/hello', [Controller::class, 'index']);
-
-
-
-//route with view
-
-// Route::get('/request', function () {
-//     return view('request');
-// });
-
-
-//route with request
- 
-// Route::any('/user', function (Request $request) {
-//     $name = $request->input("name");
-//     return $name;
-// });
-
-
-//route with 2 methode 
-// Route::match(['get', 'post'], '/', function () {
-// });
- 
-
-// route any methode
-// Route::any('/', function () {
-// });
-
-//redirect route
-// Route::redirect('/her', 'hello');
-
-
-
-//route with id
-// Route::get('/person/{id}', function ($id) {
-//     return 'person id : '.$id;
-// });
 
 
 
