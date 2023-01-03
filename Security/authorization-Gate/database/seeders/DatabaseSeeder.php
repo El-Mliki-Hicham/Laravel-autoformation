e037177db70e286781fe8b6cc876a583f4ae46d4
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-03 22:19:38 +0100

    create policy and seeders

--- Security/authorization-Gate/database/seeders/DatabaseSeeder.php
+++ Security/authorization-Gate/database/seeders/DatabaseSeeder.php
@@ -16,9 +16,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'user1',
+            'email' => 'user1@gmail.com',
+            "role" => "user",
+            "password" => "user1user1",
+
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'user2',
+            'email' => 'user2@gmail.com',
+            "role" => "user",
+            "password" => "user2user2",
+
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            "role" => "admin",
+            "password" => "adminadmin",
+
+        ]);
     }
 }
