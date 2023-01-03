ed4210fbde8f3ecb65d4b5e9cac5a7c00e347e94
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-03 20:36:11 +0100

    push laravel

--- Security/Authorization/resources/views/request.blade.php
+++
@@ -1,9 +0,0 @@
-@if (session('session'))
-<h1 style="color: rgb(252, 0, 0)">{{ session('session') }}</h1>
-
-@endif
-<form action="/user" method="post">
-    @csrf
-    <input type="text" name="name">
-    <button>click</button>
-</form>
